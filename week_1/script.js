3c947d6e5bc16a9e43521b0b28ce9c88b38f79a0
lib2024prof <38268578+lib2024prof@users.noreply.github.com>
2022-09-15 19:06:28 -0400

    Initial Commit

---
+++ week_1/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById('test-2').style.color = 'green'
+document.getElementById('test-2').style.color = 'green'
+document.getElementById('test-2').style.color = 'green'
+document.getElementById('test-2').style.color = 'green'
+
+
+/**
+ * This function is to test JS 
+ */
+function test() {}
---
+++ week_2/script.js
@@ -0,0 +1,37 @@
+// alert("Working")
+
+// Declartion NameOfVariable = Value 
+var myVar 
+// Redeclaring myVar
+var myVar = "String"
+myVar = 2
+
+// Declartion NameOfVariable = Value 
+let employeeName = ''
+employeeName = 'my Name'
+
+// Declartion NameOfVariable = Value 
+const lastName = 'Rathor'
+
+const randomString = "1+12abc---++;;;''!!"
+
+const pet = 'dogs'
+const number = 3
+
+const anotherNumber = 22.7
+
+const boolValue = true
+
+console.log(boolValue);
+console.error("This is an error")
+console.warn("Do not open")
+
+
+// concatenate any string or variables using + symbol
+console.log('I have ' + number + ' ' + pet + '.')
+
+// Template Literal 
+
+console.log(`I have ${number} 
+
+${pet}.`)
